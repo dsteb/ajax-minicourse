13774545b8aa3f2ca71aefdf755c1cbb4080a466
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-07-31 23:03:37 +0200

    refactor: Isolate code from global
    This is done to add 'use strict' to the code and
    make it not global.

--- js/script.js
+++ js/script.js
@@ -1,51 +1,57 @@
-function loadData() {
+(function() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+  'use strict';
+  
+  function loadData() {
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+      var $body = $('body');
+      var $wikiElem = $('#wikipedia-links');
+      var $nytHeaderElem = $('#nytimes-header');
+      var $nytElem = $('#nytimes-articles');
+      var $greeting = $('#greeting');
 
-    // load streetview
-    var street = $('#street').val();
-    var city = $('#city').val();
-    var address = street + ', ' + city;
-    var location = street + ',' + city;
-    $greeting.text('Do you really want to live at ' + address + '?');
-    var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + location;
-    var $bg = $('<img>').addClass('bgimg').attr('src', url);
-    $body.append($bg);
+      // clear out old data before new request
+      $wikiElem.text("");
+      $nytElem.text("");
 
-    url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-      'api-key': 'd42803f1d4a94ffe84ae633e29fbfa98',
-      q: location
-    });
+      // load streetview
+      var street = $('#street').val();
+      var city = $('#city').val();
+      var address = street + ', ' + city;
+      var location = street + ',' + city;
+      $greeting.text('Do you really want to live at ' + address + '?');
+      var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + location;
+      var $bg = $('<img>').addClass('bgimg').attr('src', url);
+      $body.append($bg);
 
-    $.getJSON(url, function(data) {
-      var title = 'New York Times articles for ' + address;
-      $nytHeaderElem.text(title);
-      var $ul = $('#nytimes-articles');
-      data.response.docs.forEach(function(doc) {
-        var $li = $('<li>').addClass('article');
-        var headline = doc.headline.print_headline || doc.headline.main;
-        var paragraph = doc.lead_paragraph;
-        if (headline && paragraph) {
-          $('<a>').text(headline).attr('href', doc.web_url).appendTo($li);
-          $('<p>').text(paragraph).appendTo($li);
-          $ul.append($li);
-        }
+      url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+      url += '?' + $.param({
+        'api-key': 'd42803f1d4a94ffe84ae633e29fbfa98',
+        q: location
       });
-    }).fail(function(err) {
-      console.error(err);
-      var title = 'New Your Times articles can not be loaded';
-      $nytHeaderElem.text(title);
-    });
-    return false;
-}
 
-$('#form-container').submit(loadData);
+      $.getJSON(url, function(data) {
+        var title = 'New York Times articles for ' + address;
+        $nytHeaderElem.text(title);
+        var $ul = $('#nytimes-articles');
+        data.response.docs.forEach(function(doc) {
+          var $li = $('<li>').addClass('article');
+          var headline = doc.headline.print_headline || doc.headline.main;
+          var paragraph = doc.lead_paragraph;
+          if (headline && paragraph) {
+            $('<a>').text(headline).attr('href', doc.web_url).appendTo($li);
+            $('<p>').text(paragraph).appendTo($li);
+            $ul.append($li);
+          }
+        });
+      }).fail(function(err) {
+        console.error(err);
+        var title = 'New Your Times articles can not be loaded';
+        $nytHeaderElem.text(title);
+      });
+      return false;
+  }
+
+  $('#form-container').submit(loadData);
+
+})();
