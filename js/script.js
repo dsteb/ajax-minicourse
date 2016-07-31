53411eee1a591af46d2aad85a2225931f0057e4f
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-07-31 21:39:57 +0200

    feat: Getting news from NewYorkTimes API

--- js/script.js
+++ js/script.js
@@ -12,12 +12,30 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    var address = $('#street').val() + ',' + $('#city').val();
-    var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address;
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ', ' + city;
+    var location = street + ',' + city;
+    $greeting.text('Do you really want to live at ' + address + '?');
+    var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + location;
     var $bg = $('<img>').addClass('bgimg').attr('src', url);
-    // YOUR CODE GOES HERE!
     $body.append($bg);
+
+    url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': 'd42803f1d4a94ffe84ae633e29fbfa98',
+      q: location
+    });
+    
+    $.getJSON(url, function(data) {
+      var $ul = $('#nytimes-articles');
+      data.response.docs.forEach(function(doc) {
+        var $li = $('<li>');
+        var headline = doc.headline.print_headline || doc.headline.main;
+        $('<h4>').text(headline).appendTo($li);
+        $ul.append($li);
+      });
+    });
     return false;
 }
 
